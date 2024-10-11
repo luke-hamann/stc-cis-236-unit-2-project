9ce9587a484064d35b879664e8c02f445cf363da
luke-hamann <135341515+luke-hamann@users.noreply.github.com>
2024-10-11 18:59:28 -0500

    Add id and nullable properties.

--- WhaleApp/Models/Whale.cs
+++ WhaleApp/Models/Whale.cs
@@ -4,40 +4,42 @@ namespace WhaleApp.Models
 {
     public class Whale
     {
+        public int? Id { get; set; }
+
         /* General Info */
 
         [Required(ErrorMessage = "Please enter a common name.")]
-        public string CommonName { get; set; }
+        public string? CommonName { get; set; }
 
         [Required(ErrorMessage = "Please enter a scientific name.")]
-        public string ScientificName { get; set; }
+        public string? ScientificName { get; set; }
 
-        public string Description { get; set; }
+        public string? Description { get; set; }
 
         [Range(0, 1000, ErrorMessage = "Lifespan must be between 0 and 1,000 years.")]
-        public int Lifespan { get; set; }
+        public int? Lifespan { get; set; }
 
         [Range(0, 50000, ErrorMessage = "Migration distance must be between 0 and 10,000 miles.")]
-        public int MigrationDistance { get; set; }
+        public int? MigrationDistance { get; set; }
 
         /* Species Status */
 
         [Range(0, 1000000, ErrorMessage = "Population must be between 0 and 1,000,000.")]
-        public int Population { get; set; }
+        public int? Population { get; set; }
 
         [Range(0, 7, ErrorMessage = "Invalid conservation status.")]
-        public int ConservationStatus { get; set; }
+        public int? ConservationStatus { get; set; }
 
         /* Location */
 
-        public bool IsInArcticOcean { get; set; }
+        public bool? IsInArcticOcean { get; set; }
 
-        public bool IsInAtlanticOcean { get; set; }
+        public bool? IsInAtlanticOcean { get; set; }
 
-        public bool IsInIndianOcean { get; set; }
+        public bool? IsInIndianOcean { get; set; }
 
-        public bool IsInPacificOcean { get; set; }
+        public bool? IsInPacificOcean { get; set; }
 
-        public bool IsInSouthernOcean { get; set; }
+        public bool? IsInSouthernOcean { get; set; }
     }
 }
