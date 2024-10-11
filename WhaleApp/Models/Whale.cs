6c5dcefbdc1b7108282ef0a1df7b826b4f0160a9
luke-hamann <135341515+luke-hamann@users.noreply.github.com>
2024-10-11 18:41:07 -0500

    Add Whale class.

---
+++ WhaleApp/Models/Whale.cs
@@ -0,0 +1,44 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WhaleApp.Models
+{
+    public class Whale
+    {
+        /* General Info */
+
+        [Required]
+        public string CommonName { get; set; }
+
+        [Required]
+        public string ScientificName { get; set; }
+
+        public string Description { get; set; }
+
+
+        [Range(0, 1000, ErrorMessage = "Lifespan must be between 0 and 1,000 years.")]
+        public int Lifespan { get; set; }
+
+        [Range(0, 50000, ErrorMessage = "Migration distance must be between 0 and 10,000 miles.")]
+        public int MigrationDistance { get; set; }
+
+        /* Species Status */
+
+        [Range(0, 1000000, ErrorMessage = "Population must be between 0 and 1,000,000.")]
+        public int Population { get; set; }
+
+        [Range(0, 7, ErrorMessage = "Invalid conservation status.")]
+        public int ConservationStatus { get; set; }
+
+        /* Location */
+
+        public bool IsInArcticOcean { get; set; }
+
+        public bool IsInAtlanticOcean { get; set; }
+
+        public bool IsInIndianOcean { get; set; }
+
+        public bool IsInPacificOcean { get; set; }
+
+        public bool IsInSouthernOcean { get; set; }
+    }
+}
