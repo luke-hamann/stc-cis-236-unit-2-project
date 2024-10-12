04f9939347b2f880f174a96db8a0478775c0e69c
luke-hamann <135341515+luke-hamann@users.noreply.github.com>
2024-10-11 20:05:05 -0500

    Start edit and detail pages.

---
+++ WhaleApp/Controllers/WhaleController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WhaleApp.Models;
+
+namespace WhaleApp.Controllers
+{
+    public class WhaleController : Controller
+    {
+        private WhaleContext context;
+
+        public WhaleController(WhaleContext context)
+        {
+            this.context = context;
+        }
+
+        [HttpGet]
+        public IActionResult Detail(int id)
+        {
+            var whale = context.Whales.Find(id);
+
+            return View(whale);
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            var whale = context.Whales.Find(id);
+            return View(whale);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Whale whale)
+        {
+            if (ModelState.IsValid)
+            {
+                context.Whales.Update(whale);
+                context.SaveChanges();
+                return RedirectToAction("Whale", "View", whale.Id);
+            }
+            else
+            {
+                return View(whale);
+            }
+        }
+    }
+}
