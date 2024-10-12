f5260d3d0813c9f7127189e4f93e2e4536389acd
luke-hamann <135341515+luke-hamann@users.noreply.github.com>
2024-10-11 19:07:02 -0500

    Enable dependency injection.

--- WhaleApp/Program.cs
+++ WhaleApp/Program.cs
@@ -1,7 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using WhaleApp.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<WhaleContext>(options =>
+    options.UseSqlServer(
+        builder.Configuration.GetConnectionString("WhaleContext")));
 
 var app = builder.Build();
 
