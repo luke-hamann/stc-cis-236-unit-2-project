9c28dafdf3d36b283da8979006678797bc12e814
luke-hamann <135341515+luke-hamann@users.noreply.github.com>
2024-10-13 12:49:34 -0500

    Add conservation status class.

---
+++ WhaleApp/Models/ConservationStatus.cs
@@ -0,0 +1,34 @@
+﻿//
+// Title: Conservation Status Class
+// Purpose: This class maps conservation status codes to their descriptions.
+//
+
+namespace WhaleApp.Models
+{
+    public static class ConservationStatus
+    {
+        public static string CodeToString(int code)
+        {
+            return code switch
+            {
+                0 => "N/A",
+                1 => "Extinct",
+                2 => "Extinct in the Wild",
+                3 => "Critically Endangered",
+                4 => "Endangered",
+                5 => "Vulnerable",
+                6 => "Near Threatened",
+                7 => "Least Concern",
+                _ => "",
+            };
+        }
+
+        public static int CodeCount
+        {
+            get
+            {
+                return 8;
+            }
+        }
+    }
+}
